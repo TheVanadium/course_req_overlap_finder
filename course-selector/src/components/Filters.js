9e996e78acd8ab5479bd62ec737087143c83252a
Garret <garretthecastro@gmail.com>
2022-12-27 09:41:26 -0800

    doc: remove unecessary development notes

--- course-selector/src/components/Filters.js
+++ course-selector/src/components/Filters.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ChecklistFilter from './ChecklistFilter.js';
 import './Filters.css';
 
-// change the above into a dictionary with the key being the badge name and the value being false by default
-// then change the onChange function to change the value of the key to the opposite of what it was before
-
 const badgeList = [
   "Ethics",
   "Global Awareness",
