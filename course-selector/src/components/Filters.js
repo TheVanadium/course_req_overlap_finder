4ceccbf167bcc0a10ee1db9ddb14903106565fde
Garret <garretthecastro@gmail.com>
2022-12-26 17:28:00 -0800

    fix: add missing badges

--- course-selector/src/components/Filters.js
+++ course-selector/src/components/Filters.js
@@ -5,7 +5,16 @@ import './Filters.css';
 // change the above into a dictionary with the key being the badge name and the value being false by default
 // then change the onChange function to change the value of the key to the opposite of what it was before
 
-const badgeList = ["Scientific Method", "Literary and Textual Analysis", "Media and Visual Analysis", "Quantitative and Numerical Analysis", "Pratical and Applied Knowledge", "Ethics"];
+const badgeList = [
+  "Scientific Method", 
+  "Literary and Textual Analysis", 
+  "Media and Visual Analysis", 
+  "Quantitative and Numerical Analysis", 
+  "Pratical and Applied Knowledge", 
+  "Ethics",
+  "Global Awareness",
+  "Sustainability",
+];
 
 function Filters(props) {
     return (
