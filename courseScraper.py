24202638823d83da8906d17e7e87c5307556c0c8
Garret <garretthecastro@gmail.com>
2022-12-27 13:28:37 -0800

    fix: span 195 getting cut off

--- courseScraper.py
+++ courseScraper.py
@@ -11,9 +11,13 @@ def getUCMercedCourses(badgeUrl):
     # course list items are in h2 Courses's next sibling div
     courseListItems = soup.find("h2", text="Courses").find_next_sibling("div").find_all("li")
 
-    # get the text of each course and remove the " Units: "... part
+    # get the text of each course and remove the "Units: " + unit number ... part
+    # SPAN 195 has a weird format excluding the exact number of units, so we manually replace it
     courses = [course.text for course in courseListItems]
     courses = [course[:-9] for course in courses]
+
+    #in courses replace "SPAN 195: Upper Division Undergraduate Resear" with "SPAN 195: Upper Division Undergraduate Research"
+    courses[courses.index("SPAN 195: Upper Division Undergraduate Resear")] = "SPAN 195: Upper Division Undergraduate Research"
 
     return courses
 
