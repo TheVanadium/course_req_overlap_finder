445d6a7f2994f370af50226f45197551a2ce18eb
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-01-01 12:23:06 -0800

    doc: badgeList

--- course-selector/src/components/Filters.js
+++ course-selector/src/components/Filters.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ChecklistFilter from './ChecklistFilter.js';
 import './Filters.css';
 
+/**
+ * For more info go to https://ue.ucmerced.edu/academics
+   This is a list of all the badges that are available, except:
+   -Leadership, Community, and Engaging the World (Units aren't listed, which messes up the API. Will need to be fixed.)
+     https://ge.ucmerced.edu/intellectual-experience-badges/leadership-community-and-engaging-world
+   -Practical and Applied Skills (No Page Available)
+   -Quantitative and Numerical Analysis (No Page Available)
+ */
+
 const badgeList = [
   "Ethics",
   "Diversity and Identity",
@@ -17,7 +26,6 @@ function Filters(props) {
       <div id="filters">
         <h2>Filters</h2>
         <ChecklistFilter checklistItems={(badgeList)} update={(value)=>{
-          console.log(value + " was updated");
           props.updateFilters(value);
         }
         }></ChecklistFilter>
