098751a5bd0c590615636dd0873bbb123d6c9161
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-01-02 10:22:14 -0800

    feat: base loading component

--- course-selector/src/components/Results.js
+++ course-selector/src/components/Results.js
@@ -10,12 +10,32 @@ function Results(props) {
       return props.courses.map((course) => <p key={course} className={"course"}>{course}</p>);
     }
 
+    function coursesLoading() {
+      // return <p id={"loading"} className={"course"}>Loading...</p>;
+      // 10 empty boxes
+      return (
+        <div id="loadingBoxList">
+          <div className={"loadingBox"}><div className={"skeletonText"}></div><div className={"skeletonText"}></div></div>
+          <div className={"loadingBox"}><div className={"skeletonText"}></div><div className={"skeletonText"}></div></div>
+          <div className={"loadingBox"}><div className={"skeletonText"}></div><div className={"skeletonText"}></div></div>
+          <div className={"loadingBox"}><div className={"skeletonText"}></div><div className={"skeletonText"}></div></div>
+          <div className={"loadingBox"}><div className={"skeletonText"}></div><div className={"skeletonText"}></div></div>
+          <div className={"loadingBox"}><div className={"skeletonText"}></div><div className={"skeletonText"}></div></div>
+          <div className={"loadingBox"}><div className={"skeletonText"}></div><div className={"skeletonText"}></div></div>
+          <div className={"loadingBox"}><div className={"skeletonText"}></div><div className={"skeletonText"}></div></div>
+          <div className={"loadingBox"}><div className={"skeletonText"}></div><div className={"skeletonText"}></div></div>
+          <div className={"loadingBox"}><div className={"skeletonText"}></div><div className={"skeletonText"}></div></div>
+        </div>
+      );
+    }
+
     return (
       <div id="results">
         <h2>Results</h2>
         <div id="courseResultsList">
           {/* if props.loading is true, put loading <p></p> otherwise generate courses */}
-          {props.loading ? <p id={"loading"} className={"course"}>Loading...</p> : generateCourses()}
+          {/* todo: make proper loading component, column bubbles with shining thing*/}
+          {props.loading ? coursesLoading() : generateCourses()}
         </div>
       </div>
     );
