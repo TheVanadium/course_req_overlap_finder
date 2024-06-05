c423642fc22444d0500c64fb0d479952ebe24643
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2024-06-05 11:31:21 -0700

    refactor: eslint, prettier
    ignoring warning because firebase put it there and i dont want to touch it

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import './App.css';
-import Filters from './components/Filters';
-import Results from './components/Results';
-import Header from './components/Header';
-import React, { useState, useEffect } from 'react';
+import "./App.css";
+import Filters from "./components/Filters";
+import Results from "./components/Results";
+import Header from "./components/Header";
+import React, { useState, useEffect } from "react";
 
 function App() {
-
   const [courses, setCourses] = useState([]);
-  const [badgeFilters, setBadgeFilters] = useState([])
+  const [badgeFilters, setBadgeFilters] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    loadCourses();
-  }, [badgeFilters])
-
-  /**
-   * Loads the courses from the server based on the badge filters.
-   */
-  function loadCourses() {
     if (badgeFilters.length === 0) {
       setCourses([]);
       return;
     }
-    // loading feature 
+    // loading feature
     setLoading(true);
-    fetch('https://thevanadium.pythonanywhere.com/courses?badges=' + badgeFilters.join(',')).then(
-      res => res.json()
-    ).then(data => {
+    fetch(
+      "https://thevanadium.pythonanywhere.com/courses?badges=" +
+        badgeFilters.join(","),
+    )
+      .then((res) => res.json())
+      .then((data) => {
         setCourses(data);
         setLoading(false);
-      }
-    )
-  }
+      });
+  }, [badgeFilters]);
 
   /**
    * Updates the badge filters based on the given badge.
@@ -39,7 +33,7 @@ function App() {
    */
   function updateFilter(badge) {
     if (badgeFilters.includes(badge)) {
-      setBadgeFilters(badgeFilters.filter(item => item !== badge));
+      setBadgeFilters(badgeFilters.filter((item) => item !== badge));
     } else {
       setBadgeFilters(badgeFilters.concat(badge));
     }
@@ -48,9 +42,17 @@ function App() {
   return (
     <div className="app-container">
       <Header></Header>
-      <div className='search-tool'>
-        <Filters updateFilters={(value) => {updateFilter(value);}}></Filters>
-        <Results courses={courses} loading={loading} badgeFilters={badgeFilters}></Results>
+      <div className="search-tool">
+        <Filters
+          updateFilters={(value) => {
+            updateFilter(value);
+          }}
+        ></Filters>
+        <Results
+          courses={courses}
+          loading={loading}
+          badgeFilters={badgeFilters}
+        ></Results>
       </div>
     </div>
   );
